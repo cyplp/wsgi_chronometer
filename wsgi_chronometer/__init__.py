cb994e55f99417e1ba8cc0ca36db947696fb8e41
Cyprien Le Pannérer <cyplp@free.fr>
2013-07-16 16:24:34 +0200

    Take arguments from conf

--- wsgi_chronometer/__init__.py
+++ wsgi_chronometer/__init__.py
@@ -1,24 +1,43 @@
 import time
+import datetime
 
 
 def chronometer_filter_factory(app, global_conf, **kwargs):
     """
 
     """
-    return ChronoFilter(app, kwargs)
+    return ChronoFilter(app, **kwargs)
 
 
 class ChronoFilter(object):
-    def __init__(self, app, args):
+    def __init__(self, app, **kwargs):
         self._app = app
-        self._args = args
 
+        if 'separator' in kwargs:
+            self._sep = kwargs['separator']
+        else:
+            self._separator = '-'
+
+        if 'fields' in kwargs:
+            self._fields = kwargs['fields'].split()
+        else:
+            self._fields = []
+            
     def __call__(self, environ, start_response):
         begin = time.time()
         result = self._app(environ, start_response)
         end = time.time()
-        # TODO factor this, use logging ?
-        print environ['REQUEST_METHOD'], end - begin, environ['PATH_INFO']
+        
+        data = [str(datetime.datetime.now())]
+        for field in self._fields:
+            try:
+                data.append(environ[field])
+            except KeyError:
+                data.append(' ')
+                
+        data.append(str((end- begin) * 1000))
+        sep = " %s " % (self._sep)
+        print sep.join(data)
         return result
 
 
