2dadd6d22e78cf082c7b0409f88c4d4fbe7d907f
Cyprien Le Pannérer <cyplp@free.fr>
2013-07-18 08:29:33 +0200

    some code testing

--- wsgi_chronometer/__init__.py
+++ wsgi_chronometer/__init__.py
@@ -94,27 +94,36 @@ class ChronoFilter(object):
         print sep.join(data)
         return result
 
+# class ProxyToFields(object):
+#     req = None
+#     resp = None
+#     end = None
+#     begin = None
 
-""" {'webob._parsed_query_vars': (GET([]), ''),
-'SERVER_SOFTWARE': 'waitress',
-'SCRIPT_NAME': '',
-'REQUEST_METHOD': 'GET',
-'PATH_INFO': '/',
-'SERVER_PROTOCOL': 'HTTP/1.1',
-'QUERY_STRING': '',
-'HTTP_USER_AGENT':
-'curl/7.21.6 (x86_64-pc-linux-gnu) libcurl/7.21.6 OpenSSL/1.0.0e zlib/1.2.3.4 libidn/1.22 librtmp/2.3',
-'SERVER_NAME': 'localhost',
-'REMOTE_ADDR': '127.0.0.1',
-'wsgi.url_scheme': 'http',
-'SERVER_PORT': '6543',
-'wsgi.input': <_io.BytesIO object at 0x2153770>,
-'HTTP_HOST': '0.0.0.0:6543',
-'wsgi.multithread': True,
-'HTTP_ACCEPT': '*/*',
-'wsgi.version': (1, 0),
-'wsgi.run_once': False,
-'wsgi.errors': <open file '<stderr>', mode 'w' at 0x7fd726114270>,
-'wsgi.multiprocess': False,
-'wsgi.file_wrapper': <class 'waitress.buffers.ReadOnlyFileBasedBuffer'>,
-'webob._parsed_cookies': ({}, '')}"""
+#     _fields = {
+#             'SERVER_SOFTWARE': None, # 'waitress'
+#             'SCRIPT_NAME': req.script_name,
+#             'REQUEST_METHOD': req.method,
+#             'PATH_INFO': req.path_info,
+#             'SERVER_PROTOCOL': req.http_version,
+#             'QUERY_STRING': req.query_string,
+#             'HTTP_USER_AGENT': req.user_agent,
+#             'SERVER_NAME': req.server_name,
+#             'REMOTE_ADDR': req.remote_addr,
+#             'wsgi.url_scheme': req.scheme,
+#             'SERVER_PORT': str(req.server_port),
+
+#             'HTTP_HOST': req.host,
+# #            'wsgi.multithread': True, # TODO
+#             'HTTP_ACCEPT': str(req.accept),
+# #            'wsgi.version': (1, 0),
+# #            'wsgi.run_once': False,
+
+# #            'wsgi.multiprocess': False,
+# #            'webob._parsed_cookies': ({}, '')
+#             'STATUS': resp.status,
+#             'STATUS_CODE': resp.status_code,
+#             'DATETIME': str(datetime.datetime.now()),
+#             'TIME': "%.02fms" % ((end- begin) * 1000),
+#             }
+
